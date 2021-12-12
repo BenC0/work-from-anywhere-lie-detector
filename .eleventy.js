89400bd404c223e42210ed12e7b4644f6d59fcdf
BenC0 <suddensquirrel94@gmail.com>
2021-12-12 21:21:02 +0000

    Integrated PostCSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,21 @@
+const postCss = require("postcss");
+
 module.exports = function(eleventyConfig) {
     // Return your Object options:
+    eleventyConfig.addNunjucksAsyncFilter('postcss', (cssCode, done) => {
+      postCss([
+        require('postcss-nested'),
+      ])
+      .process(cssCode)
+      .then(
+        r => done(null, r.css),
+        e => done(e, null)
+      )
+    })
+    eleventyConfig.addWatchTarget("src/**/*.css");
     return {
+      passthroughFileCopy: true,
+      markdownTemplateEngine: "njk",
       dir: {
         input: "src",
         output: "dist"
